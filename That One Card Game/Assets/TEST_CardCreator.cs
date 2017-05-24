5b41a6965d36636e0b34280fe63d2f608e25f379
ritoban123 <ritobanrc@gmail.com>
2017-05-24 14:23:28 -0700

    Same as last commit.

---
+++ That One Card Game/Assets/TEST_CardCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_CardCreator : MonoBehaviour
+{
+
+    public GameObject CardHolder;
+    public int CardCount;
+
+    private void Awake()
+    {
+        for (int i = 0; i < CardCount; i++)
+        {
+            GameObject go = Instantiate(CardHolder, this.transform);
+            go.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0.8f, 1, 0.8f, 1);
+        }
+    }
+}
